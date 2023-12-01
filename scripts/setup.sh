d5374aeea5400a492f01adb4d0531fcb58250c5c
David Bonham <david@davidbonham.org.uk>
2023-12-01 10:17:47 +0000

    Define environment variables that cintsys actually needs
    Despite the comments in the readme file, we still need to define
    all of the paths for the 64 bit world rather than just BCPLROOT
    and PATH. Do so.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,3 +5,8 @@ alias @="cintsys64 -c"
 export BL_ROOT=/home/david/bcpl-llvm
 PATH=/usr/lib/llvm-17/bin:${PATH}
 
+
+export BCPL64ROOT=/home/david/official_bcpl_release/BCPL/cintcode
+export BCPL64PATH=$BCPL64ROOT/cin64
+export BCPL64HDRS=$BCPL64ROOT/g
+export BCPL64SCRIPTS=$BCPL64ROOT/s
