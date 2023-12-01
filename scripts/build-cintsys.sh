8b90ccd7f63ce225dfe3c0a4c071b5783ea8356f
David Bonham <david@davidbonham.org.uk>
2023-12-01 13:13:55 +0000

    build script now working using unmodified releases of bcpl and llvm as inputs

--- scripts/build-cintsys.sh
+++ scripts/build-cintsys.sh
@@ -6,12 +6,30 @@
 
 # Compile our local extfn64.c
 
+if [[ -z "${BCPLROOT}" ]]; then
+    echo BCPLROOT is unset
+    exit
+elif [[ -z "${BL_ROOT}" ]]; then
+    echo BL_ROOT is unset
+    exit
+fi
+
 OBJ=${BCPLROOT}/obj64
 echo OBJ=$OBJ
 
-gcc -o ${BL_ROOT}/bin/cintsys64 ${OBJ}/cintmain.o ${OBJ}/cinterp.o ${OBJ}/fasterp.o \
-       ${OBJ}/kblib.o ${OBJ}/cfuncs.o ${OBJ}/joyfn.o ${OBJ}/sdlfn.o ${OBJ}/glfn.o ${OBJ}/extfn.o \
-       ${OBJ}/llvm_bcpl_binding64.o \
-       `llvm-config --ldflags --libs` -pthread -ltinfo -lz -lm -lstdc++
+
+# Create the LLVM C binding
+gcc -I ${BCPLROOT}/sysc -I ${BL_ROOT}/src/inc -I ${BL_ROOT}/src/c-api -I `llvm-config --includedir` \
+    -o ${BL_ROOT}/obj/llvm_bcpl_binding.o -c ${BL_ROOT}/src/llvm_bcpl_binding.c
+
+# Create our bespoke cintsys dispatcher for external functions
+gcc -DEXTavail -I ${BCPLROOT}/sysc -I ${BL_ROOT}/src/c-api -o ${BL_ROOT}/obj/extfn.o -c ${BL_ROOT}/src/extfn.c
+
+gcc  -Xlinker -Map=/tmp/output.map -o ${BL_ROOT}/bin/cintsys64  ${OBJ}/cinterp.o ${OBJ}/fasterp.o \
+       ${OBJ}/kblib.o ${OBJ}/cfuncs.o \
+       ${OBJ}/joyfn.o ${OBJ}/sdlfn.o ${OBJ}/glfn.o ${OBJ}/alsafn.o \
+       ${BL_ROOT}/obj/extfn.o \
+       ${BL_ROOT}/obj/llvm_bcpl_binding.o ${OBJ}/cintmain.o \
+       `llvm-config --ldflags --libs` -pthread -ltinfo  -lm -lstdc++
 
 
