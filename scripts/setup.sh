833311d2d634939bfc149eae32f5dcc994f43beb
David Bonham <david@davidbonham.org.uk>
2023-12-05 20:24:33 +0000

    set up @ alias correctly

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,3 @@
-alias @="${BL_ROOT}/bin/cintsys64 -c"
 
 export BL_ROOT=/home/david/bcpl-llvm
 PATH=/usr/lib/llvm-17/bin:${PATH}
@@ -8,3 +7,4 @@ export BCPL64ROOT=/home/david/official_bcpl_release/BCPL/cintcode
 export BCPL64PATH=$BCPL64ROOT/cin64
 export BCPL64HDRS=$BCPL64ROOT/g
 export BCPL64SCRIPTS=$BCPL64ROOT/s
+alias @="${BL_ROOT}/bin/cintsys64 -c"
