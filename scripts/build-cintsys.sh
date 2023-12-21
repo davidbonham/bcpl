4fcdbc680e9e7d828ab2853bf3b7ef436bd116ca
David Bonham <david@davidbonham.org.uk>
2023-12-21 13:27:36 +0000

    Add option to build with personal debugging-enabled llvm build

--- scripts/build-cintsys.sh
+++ scripts/build-cintsys.sh
@@ -6,6 +6,16 @@
 
 # Compile our local extfn64.c
 
+# Use the release or my own debug build. The debug build is built like this,
+# positioned in the top directory of the cloned LLVM tree:
+#
+# cmake -S llvm -B build -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=DEBUG -DCMAKE_INSTALL_PREFIX=/home/david/llvm-debug-install -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_PARALLEL_COMPILE_JOBS=4 -DLLVM_PARALLEL_LINK_JOBS=4 /home/david/llvm-project-17.0.6.src
+# cmake --build build -j 4
+# make -C build install
+
+LLVMBIN=/usr/lib/llvm-17/bin
+#LLVMBIN=/home/david/llvm-debug-install/bin
+
 if [[ -z "${BCPLROOT}" ]]; then
     echo BCPLROOT is unset
     exit
@@ -16,20 +26,20 @@ fi
 
 OBJ=${BCPLROOT}/obj64
 echo OBJ=$OBJ
-
+echo  `${LLVMBIN}/llvm-config --includedir`
 
 # Create the LLVM C binding
-gcc -DforLinux64 -I ${BCPLROOT}/sysc -I ${BL_ROOT}/src/inc -I ${BL_ROOT}/src/c-api -I `llvm-config --includedir` \
+gcc -g -O0 -DforLinux64 -I ${BCPLROOT}/sysc -I ${BL_ROOT}/src/inc -I ${BL_ROOT}/src/c-api -I `${LLVMBIN}/llvm-config --includedir` \
     -o ${BL_ROOT}/obj/llvm_bcpl_binding.o -c ${BL_ROOT}/src/llvm_bcpl_binding.c
 
 # Create our bespoke cintsys dispatcher for external functions
-gcc -DEXTavail  -DforLinux64 -I ${BCPLROOT}/sysc -I ${BL_ROOT}/src/c-api -o ${BL_ROOT}/obj/extfn.o -c ${BL_ROOT}/src/extfn.c
+gcc -g -O0  -DEXTavail  -DforLinux64 -I ${BCPLROOT}/sysc -I ${BL_ROOT}/src/c-api -o ${BL_ROOT}/obj/extfn.o -c ${BL_ROOT}/src/extfn.c
 
-gcc  -Xlinker -Map=/tmp/output.map -o ${BL_ROOT}/bin/cintsys64  ${OBJ}/cinterp.o ${OBJ}/fasterp.o \
+gcc  -g -O0 -Xlinker -Map=/tmp/output.map -o ${BL_ROOT}/bin/cintsys64  ${OBJ}/cinterp.o ${OBJ}/fasterp.o \
        ${OBJ}/kblib.o ${OBJ}/cfuncs.o \
        ${OBJ}/joyfn.o ${OBJ}/sdlfn.o ${OBJ}/glfn.o ${OBJ}/alsafn.o \
        ${BL_ROOT}/obj/extfn.o \
        ${BL_ROOT}/obj/llvm_bcpl_binding.o ${OBJ}/cintmain.o \
-       `llvm-config --ldflags --libs` -pthread -ltinfo  -lm -lstdc++
+       `${LLVMBIN}/llvm-config --ldflags --libs` -pthread -ltinfo  -lm -lstdc++
 
 
