7976c44990578856e5043ffe04dcd1234059b201
David Bonham <david@davidbonham.org.uk>
2024-02-29 15:34:56 +0000

    Teach the setup script about the location on different machines

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,32 @@
+#!/usr/bin/env bash
+#
+# Set things up for BCPL development
 
-export BL_ROOT=/home/david/bcpl-llvm
+if [ -z "${DEVROOT}" ]
+then
+    case $(hostname) in
+        copper)
+            export DEVROOT=/home/david
+            ;;
+        vanadium)
+            export DEVROOT=/mnt/data/usr/fs/bcpl-development
+            ;;
+
+        *)
+            echo unknown host - set up DEVROOT manually
+            return 1
+            ;;
+    esac
+fi 
+
+export BL_ROOT=${DEVROOT}/bcpl-llvm
 PATH=/usr/lib/llvm-17/bin:${PATH}
 
-export BCPLROOT=/home/david/official_bcpl_release/BCPL/cintcode
-export BCPL64ROOT=/home/david/official_bcpl_release/BCPL/cintcode
-export BCPL64PATH=$BCPL64ROOT/cin64
-export BCPL64HDRS=$BCPL64ROOT/g
-export BCPL64SCRIPTS=$BCPL64ROOT/s
+export BCPLROOT=${DEVROOT}/official_bcpl_release/BCPL/cintcode
+export BCPL64ROOT=${DEVROOT}/official_bcpl_release/BCPL/cintcode
+export BCPL64PATH=${BCPL64ROOT}/cin64
+export BCPL64HDRS=${BCPL64ROOT}/g
+export BCPL64SCRIPTS=${BCPL64ROOT}/s
 alias @="${BL_ROOT}/bin/cintsys64 -c"
+
+
