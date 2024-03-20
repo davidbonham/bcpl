b42fb4c0ea5e9ae0124e189c4b7ecf3dcd4a09c6
David Bonham <david@davidbonham.org.uk>
2024-03-20 21:01:13 +0000

    Start adding comments identifying missing elements of the global vector

--- rtl/bcplinit.s
+++ rtl/bcplinit.s
@@ -1,3 +1,7 @@
+# The standard BCPL Global Vector
+#
+# This corresponds to the values in cintcode/g/libhdr.h
+
     .section BCPLGVEC,"aw",@progbits
     .global __bcpl_global_vector
     .global __bcpl_elaborators
@@ -7,43 +11,43 @@
 
 
  __bcpl_global_vector:
-    .quad 0                # 0
-    .quad 0                # 1     
-    .quad __stop           # 2 STOP is called by the FINISH O-code operator
-    .quad 0                # 3
-    .quad 0                # 4
+    .quad 0                # 0      glob0 = globsize
+    .quad 0                # 1      START
+    .quad __stop           # 2      STOP is called by the FINISH O-code operator
+    .quad 0                # 3      (sys)
+    .quad 0                # 4      (clihook)
     .quad __muldiv         # 5
-    .quad 0                # 6
-    .quad 0                # 7
-    .quad 0                # 8
-    .quad 0                # 9
-    .quad 0                # 10
-    .quad 0                # 11
-    .quad 0                # 12 CIS
-    .quad 0                # 13 COS
-    .quad 0                # 14
-    .quad 0                # 15
-    .quad 0                # 16
-    .quad 0                # 17
-    .quad 0                # 18
-    .quad 0                # 19
-    .quad 0                # 20 
-    .quad 0                # 21 
-    .quad 0                # 22
-    .quad 0                # 23
-    .quad 0                # 24
-    .quad 0                # 25
-    .quad 0                # 26
-    .quad 0                # 27
-    .quad 0                # 28
-    .quad 0                # 29
+    .quad 0                # 6      (changeco)
+    .quad 0                # 7      (currco)
+    .quad 0                # 8      (colist)
+    .quad 0                # 9      (rootnode)
+    .quad 0                # 10     RESULT2
+    .quad 0                # 11     (tempval)
+    .quad 0                # 12     CIS
+    .quad 0                # 13     COS
+    .quad 0                # 14     (currentdir)
+    .quad 0                # 15     LEVEL
+    .quad 0                # 16     LONGJUMP
+    .quad 0                # 17     (createco)
+    .quad 0                # 18     (deleteco)
+    .quad 0                # 19     (callco)
+    .quad 0                # 20     (cowait)
+    .quad 0                # 21     (resumeco)
+    .quad 0                # 22     (initco)
+    .quad 0                # 23     (startco)
+    .quad 0                # 24     (globin)
+    .quad 0                # 25     GETVEC
+    .quad 0                # 26     (rdargs2)
+    .quad 0                # 27     FREEVEC
+    .quad 0                # 28     (abort)
+    .quad 0                # 29     (sysabort)
     .quad __packstring     # 30
     .quad __unpackstring   # 31
-    .quad 0                # 32
-    .quad 0                # 33
-    .quad 0                # 34
-    .quad 0                # 35
-    .quad 0                # 36
+    .quad 0                # 32     (getword)
+    .quad 0                # 33     (putword)
+    .quad 0                # 34     RANDNO
+    .quad 0                # 35     (setseed)
+    .quad 0                # 36     (sardch)
     .quad 0                # 37
     .quad __rdch           # 38
     .quad 0                # 39
@@ -75,6 +79,6 @@
     .quad __rewind         # 65
 
     .org __bcpl_global_vector + 512*8
-__bcpl_elaborators:   
-   
+__bcpl_elaborators:
+
 
