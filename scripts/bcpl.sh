d68d32cad9777804fdf8bb07797ae03f7d555e50
David Bonham <david@davidbonham.org.uk>
2025-05-15 15:29:54 +0100

    Tell clang we are not position-independent at the moment

--- scripts/bcpl.sh
+++ scripts/bcpl.sh
@@ -167,7 +167,7 @@ do
     esac
 done
 
-perform "clang -target x86_64-unknown-linux-gnu -Wl,-z,noexecstack $optimise -g3 $source $compile $clang_prefix $clang_inputs $clang_suffix $object $map"
+perform "clang -no-pie -target x86_64-unknown-linux-gnu -Wl,-z,noexecstack $optimise -g3 $source $compile $clang_prefix $clang_inputs $clang_suffix $object $map"
 if [ $keep -eq 0 ]
 then
     perform "rm --force --verbose $ll_path $s_path $name.map"
