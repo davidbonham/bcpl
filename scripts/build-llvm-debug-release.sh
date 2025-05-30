0ee139238e9f65c1680866f1f14a664644183633
David Bonham <david@davidbonham.org.uk>
2025-05-30 14:56:35 +0100

    Update llvm installation script to build either debug or release versions

--- scripts/build-llvm-debug-release.sh
+++ scripts/build-llvm-debug-release.sh
@@ -2,7 +2,27 @@
 #
 # sudo apt install valgrind
 # sudo snap install --classic cmake
-#
+
+# Check that if an argument is passed, it is 'debug' or 'release'. By default,
+# we assume debug.
+umode=DEBUG
+
+if [[ $# -eq 1 ]]
+then
+    umode="${1^^}"
+    if [[ "${umode}" != "DEBUG" ]] && [[ "${umode}" != "RELEASE" ]]
+    then
+        echo error: expected optional parameter DEBUG or RELEASE
+        exit 1
+    fi
+elif [[ $# -ne 0 ]]
+then
+    echo error: expected optional parameter DEBUG or RELEASE
+    exit 1
+fi
+mode="${umode,,}"
+builddir=llvm-build-${mode}
+
 # Download the following release of LLVM and build a debug version, installing
 # it in llvm-debug-install. You should be in the development root (the parent
 # of bcpl-llvm) at this point
@@ -17,6 +37,8 @@ else
     export DEVROOT=$(pwd)
     export LLVM_VERSION=20.1.3
     export LLVMREL=llvm-project-${LLVM_VERSION}
+
+    echo === Building ${mode} version of ${LLVM_VERSION}
 
     # Prepare the source 
     if [ ! -d ${LLVMREL}.src ]
@@ -35,51 +57,51 @@ else
     fi
 
     # Preserve any existing llvm-build directory
-    if [ -d llvm-build ] 
+    if [ -d ${builddir} ]
     then
-        if [ -d llvm-build.old ]
+        if [ -d ${builddir}.old ]
         then
             # We want to preseve llvm-build in llvm-build-old but it already
             # exists. I really should clean things up
-            echo \*\*\* Unable to preserve llvm-build - do some tidying up:
-            ls -ltrd llvm-build*
+            echo \*\*\* Unable to preserve ${builddir} - do some tidying up:
+            ls -ltrd ${builddir}*
         else 
-            echo === Preserving llvm-build in llvm-build-old
-            mv llvm-build llvm-build-old
+            echo === Preserving ${builddir} in ${builddir}-old
+            mv ${builddir} ${builddir}-old
         fi
     fi
 
     # Build llvm
     echo === Configuring  ${LLVMREL}.src/llvm
-    mkdir llvm-build
-    cmake -B llvm-build -G "Unix Makefiles"                  \
-        -DLLVM_BUILD_TOOLS=NO                                \
-        -DLLVM_ENABLE_TERMINFO=NO                            \
-        -DLLVM_TARGETS_TO_BUILD=llvm                         \
-        -DCMAKE_BUILD_TYPE=DEBUG                             \
-        -DCMAKE_INSTALL_PREFIX=${DEVROOT}/llvm-debug-install \
-        -DCMAKE_BUILD_WITH_INSTALL_RPATH=TRUE                \
-        -DLLVM_TARGETS_TO_BUILD=host                         \
-        -DCMAKE_C_COMPILER=clang-20                          \
+    mkdir ${builddir}
+    cmake -B ${builddir} -G "Unix Makefiles"                    \
+        -DLLVM_BUILD_TOOLS=NO                                   \
+        -DLLVM_ENABLE_TERMINFO=NO                               \
+        -DLLVM_TARGETS_TO_BUILD=llvm                            \
+        -DCMAKE_BUILD_TYPE=$umode                               \
+        -DCMAKE_INSTALL_PREFIX=${DEVROOT}/llvm-${mode}-install  \
+        -DCMAKE_BUILD_WITH_INSTALL_RPATH=TRUE                   \
+        -DLLVM_TARGETS_TO_BUILD=host                            \
+        -DCMAKE_C_COMPILER=clang-20                             \
         ${LLVMREL}.src/llvm
 
     echo === Building 
-    cmake --build llvm-build -j 4
-    cmake --build llvm-build --target llvm-config
+    cmake --build ${builddir} -j 4
+    cmake --build ${builddir} --target llvm-config
 
     # Install
     echo === Installing 
 
-    make -C llvm-build install
-    cp llvm-build/bin/llvm-config ${DEVROOT}/llvm-debug-install/bin/
+    make -C ${builddir} install
+    cp ${builddir}/bin/llvm-config ${DEVROOT}/llvm-${mode}-install/bin/
 
     # Clean up
-    if [ "$1" = "clean" ]
+    if [ "$2" = "clean" ]
     then
         echo === Cleaning up
         rm -r ${LLVMREL}.src 
         rm /tmp/${LLVMREL}.src.tar.xz
-        rm -r llvm-build
+        rm -r ${builddir}
     fi
 
     echo === Done
